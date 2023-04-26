90d744745c9e8d32f3efa707a83620cadce3ce13
Alex Wildman <alexanderwildman@letu.edu>
2023-04-26 16:13:27 -0500

    Start of TUI branch

--- Client/client.go
+++ Client/client.go
@@ -1,42 +1,42 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"net"
-)
+// import (
+// 	"fmt"
+// 	"net"
+// )
 
-func main() {
-	//connect
-	conn, err := net.Dial("tcp", "127.0.0.1:2000")
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-	go print_Recieve(conn)
-	for {
-		command := getUserInput()
-		conn.Write([]byte(command))
+// func main() {
+// 	//connect
+// 	conn, err := net.Dial("tcp", "127.0.0.1:2000")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer conn.Close()
+// 	go print_Recieve(conn)
+// 	for {
+// 		command := getUserInput()
+// 		conn.Write([]byte(command))
 
-	}
+// 	}
 
-}
+// }
 
-func print_Recieve(conn net.Conn) {
-	for {
-		reply := readString(conn)
-		fmt.Println(reply)
-	}
-}
+// func print_Recieve(conn net.Conn) {
+// 	for {
+// 		reply := readString(conn)
+// 		fmt.Println(reply)
+// 	}
+// }
 
-func getUserInput() string {
-	var name string
-	fmt.Scan(&name)
-	return name
-}
+// func getUserInput() string {
+// 	var name string
+// 	fmt.Scan(&name)
+// 	return name
+// }
 
-func readString(conn net.Conn) string {
-	reply := make([]byte, 1024)
-	n, _ := conn.Read(reply)
-	reply_string := string(reply[:n])
-	return reply_string
-}
+// func readString(conn net.Conn) string {
+// 	reply := make([]byte, 1024)
+// 	n, _ := conn.Read(reply)
+// 	reply_string := string(reply[:n])
+// 	return reply_string
+// }
--- Client/main.go
+++ Client/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+var app = tview.NewApplication()
+var text = tview.NewTextView().
+	SetTextColor(tcell.ColorGreen).
+	SetText("(q) to quit")
+
+func main() {
+
+	if err := app.SetRoot(text, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
