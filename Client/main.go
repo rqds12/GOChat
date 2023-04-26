b6e29654ae2c48f0923693b2485d250153c16e42
Alex Wildman <alexanderwildman@letu.edu>
2023-04-26 17:13:09 -0500

    Set up basic login page

--- Client/main.go
+++ Client/main.go
@@ -5,14 +5,34 @@ import (
 	"github.com/rivo/tview"
 )
 
+var goColor = tcell.NewHexColor(0x007d9c)
+var userNameForm = tview.NewForm()
 var app = tview.NewApplication()
-var text = tview.NewTextView().
-	SetTextColor(tcell.ColorGreen).
-	SetText("(q) to quit")
+var pages = tview.NewPages()
+
+func connect() {
+
+}
+
+// Function to populate the form with inputs
+func setupUserNameForm() {
+	userName := ""
+	userNameForm.SetButtonsAlign(tview.AlignCenter)
+	userNameForm.AddInputField("Username", "", 50, nil, func(enteredUserName string) {
+		userName = enteredUserName
+	})
+	_ = userName
+	userNameForm.AddButton("Connect", connect)
+	userNameForm.AddButton("Exit", func() {
+		app.Stop()
+	})
+	userNameForm.SetBorder(true).SetBorderColor(goColor).SetTitle("GOChat")
+}
 
 func main() {
-
-	if err := app.SetRoot(text, true).EnableMouse(true).Run(); err != nil {
+	setupUserNameForm()
+	pages.AddPage("Login", userNameForm, true, true)
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 }
